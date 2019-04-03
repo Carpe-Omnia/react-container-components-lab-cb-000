1028959cb5045c3c16655e32fc611ac3bab5d516
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-03 01:05:01 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,9 @@
-// Code MovieReviews Here
+import React, { Component } from 'react';
+
+const MovieReviews = (props) => {
+  return (
+    <div className="movie reviews" >
+      <h2> movie review </h2>
+    </div>
+  )
+}
