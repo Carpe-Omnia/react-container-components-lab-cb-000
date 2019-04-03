433e16f7cf55e8731a98787f2c125350c0b5a635
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-03 01:19:22 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -8,6 +8,14 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
 
 // Code LatestMovieReviewsContainer Here
 class LatestMovieReviewsContainer extends React.Component {
+  constructor(){
+    super();
+    fetch(url)
+    .then(res => res.json())
+    .then(function(json){
+      console.log(json)
+    })
+  }
   render(){
     return (
       <div className="latest-movie-reviews" >
