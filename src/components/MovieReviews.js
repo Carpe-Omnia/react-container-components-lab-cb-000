4fe9455000d5dd8b8f4440f72c2545fd44b71014
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-03 01:44:30 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -4,6 +4,11 @@ const MovieReviews = (props) => {
   return (
     <div className="movie reviews" >
       <h2> movie review </h2>
+      {props.reviews.map((review) =>{
+        <p> {review.headline} </p>
+        <p> {review.display_title} </p>
+        <p> {review.link.url} </p>
+      })}
     </div>
   )
 }
