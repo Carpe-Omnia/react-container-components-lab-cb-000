be9bf07dfad7084b1078587773b70b2a67902a99
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-03 01:15:49 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,13 +6,20 @@ const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+
 class SearchableMovieReviewsContainer extends React.Component {
+  handleSubmit(event){
+
+  }
   render(){
     return (
       <div className="searchable-movie-reviews" >
         <h1>Searchable movie container </h1>
         <MovieReviews />
+        <form onSubmit={event => this.handleSubmit(event)} >
+          <input type="text" />
+          <input type="submit" />
+        </form>
       </div>
     )
   }
